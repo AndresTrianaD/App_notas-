489b8e1fe0a345b42ae576d4efeb235116461f46
Andres Triana <73327110+AndresTrianaD@users.noreply.github.com>
2023-11-29 21:32:52 -0500

    Hola mundo.
    Primer commit.

---
+++ src/drawer/DrawerNavigation.js
@@ -0,0 +1,15 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+
+const DrawerNavigation = () => {
+  return (
+    <ScrollView>
+        <View>
+         <Text>Hola mundo</Text>
+        </View>
+    </ScrollView>
+   
+  )
+}
+
+export default DrawerNavigation
